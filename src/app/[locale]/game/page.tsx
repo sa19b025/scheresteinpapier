356fd745e8a13c29d1af5c23cdf673900cccc216
Thomas Scheiber <66697291+t-scheiber@users.noreply.github.com>
2024-03-11 18:05:38 +0100

    webcam stream component added

--- src/app/[locale]/game/page.tsx
+++ src/app/[locale]/game/page.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { useTranslations } from "next-intl";
 import WebcamStream from "@/components/WebcamStream";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Game() {
   const t = useTranslations("game");
   return (
-    <div className="flex h-[80vh] items-center justify-center">
+    <div className="flex h-[80vh] flex-col items-center justify-center">
       <h1 className="">{t("title")}</h1>
       <WebcamStream />
-      <p>{t("description")}</p>
+      <p className="mb-4">{t("description")}</p>
+      <p>{t("predictionText")}</p>
+      <Button variant="secondary" className="mt-4 bg-gray-300 p-4">
+        <Image
+          src="/diaphragm.png"
+          alt={t("kamera")}
+          width={30}
+          height={30}
+          className="mx-2"
+        />
+      </Button>
     </div>
   );
 }
--- src/components/WebcamStream.tsx
+++ src/components/WebcamStream.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
 
-function WebcamStream() {
-  return <div>test</div>;
-}
+import React, { useRef, useState } from "react";
+import { useTranslations } from "next-intl";
+import Webcam from "react-webcam";
+
+type Props = {};
+
+const WebcamStream = (props: Props) => {
+  const webcamRef = useRef<Webcam>(null);
+  const [mirrored, setMirrored] = useState<boolean>(false);
+  return (
+    <div className="m-4 h-[60vh] w-[80vw] items-center text-center">
+      <div className="relative">
+        <div className="relative h-full w-full">
+          <Webcam
+            ref={webcamRef}
+            mirrored={mirrored}
+            className="mx-auto h-fit w-fit"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default WebcamStream;
