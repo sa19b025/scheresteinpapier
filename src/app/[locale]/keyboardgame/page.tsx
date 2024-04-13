2030d5657ea5b6651a5501f034322093959b20b7
Thomas Scheiber <66697291+t-scheiber@users.noreply.github.com>
2024-04-13 11:48:24 +0200

    final fixes

--- src/app/[locale]/keyboardgame/page.tsx
+++ src/app/[locale]/keyboardgame/page.tsx
@@ -1,33 +1,26 @@
 "use client";
 
 import KeyboardComponent from "@/components/KeyboardComponent";
-import { usePredictionStore } from "@/components/variableState";
+import { useLocale } from "next-intl";
 
 function KeyboardgamePage() {
-  const { setPrediction } = usePredictionStore();
+  const locale = useLocale();
+  const resultUrl = "/" + locale + "/result";
 
-  const page = document.getElementById("page");
-
-  if (page) {
-    page.addEventListener("keydown", function (event) {
-      const key = event.key;
-      if (key === "r") {
-        setPrediction("rock");
-        window.open("/results", "_blank");
-      }
-      if (key === "p") {
-        setPrediction("paper");
-        window.open("/results", "_blank");
-      }
-      if (key === "s") {
-        setPrediction("scissors");
-        window.open("/results", "_blank");
-      }
-    });
-  }
+  addEventListener("keydown", (e) => {
+    if (e.key === "r") {
+      window.location.href = resultUrl + "?selectedValue=Rock";
+    }
+    if (e.key === "p") {
+      window.location.href = resultUrl + "?selectedValue=Paper";
+    }
+    if (e.key === "s") {
+      window.location.href = resultUrl + "?selectedValue=Scissors";
+    }
+  });
 
   return (
-    <div id="page">
+    <div>
       <KeyboardComponent />;
     </div>
   );
